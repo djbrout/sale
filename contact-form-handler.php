0fcbb23f1315363bcf48f3cb2c9dbe3b55cbfad4
djbrout <djbrout@gmail.com>
2017-01-26 00:52:30 -0500

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -24,7 +24,8 @@ if( empty($errors))
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	
+	" Here are the details:\n Item: $_GET['query'] \n Name: $name \n Email: $email_address \n Message \n $message"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
